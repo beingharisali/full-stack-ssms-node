d92fc9949a14b9ccdaad2cb7474022ae89c1b08f
beingharisali <theharis99@gmail.com>
2025-11-03 11:13:25 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res)=>{
+    res.status(200).json({
+        success:true,
+        msg:'SSMS API WORKING'
+    })
+})
+
+app.listen(port, ()=>{
+    console.log(`Application is up and running on port ${port}`)
+})
