9cb1448d7675f1d137d98885d64d6fb62c835a37
Iqra yasmeen <yasirali4363@gmail.com>
2025-11-04 17:54:56 +0500

    create DB and Schema

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 require('dotenv').config()
+const port = process.env.PORT;
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 5000;
+const connecDB = require("./database/connect")
+const mongo_uri = "mongodb://localhost:27017/ssms-application"
+connecDB();
+app.use(express.json());
 
-app.get('/', (req, res)=>{
-    res.status(200).json({
-        success:true,
-        msg:'SSMS API WORKING'
-    })
-})
-
-app.listen(port, ()=>{
-    console.log(`Application is up and running on port ${port}`)
-})
+app.get("/", (req, res) => {
+	res.status(200).json({
+		success: true,
+		msg: "API working",
+	});
+});
+app.listen(port, () => {
+	console.log(`Server is up and running on port ${port}`);
+});
---
+++ models/users.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const userSchema = mongoose.Schema({
+    fullName:{
+        type: String,
+        required: true,
+        minLength: 3,
+    },
+    email: {
+        type: String,
+        required:true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+        minLength: 8,
+    },
+     role: {
+      type: String,
+      enum: ["admin", "agent", "client"],
+      default: "client",
+    },
+});
+
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
+
+
